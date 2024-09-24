9073d9fff96445e26f60e43d5d302f576fe51447
dengkui123 <1509963767@qq.com>
2024-09-24 18:01:53 +0800

    part1部分提交

---
+++ part1-React/exercises1/components/Total/index.jsx
@@ -0,0 +1,10 @@
+const Total = (props) => {
+  return (
+    <p>
+      Number of exercises{" "}
+      {props.parts.reduce((sum, part) => sum + part.exercises, 0)}
+    </p>
+  );
+};
+
+export default Total;
